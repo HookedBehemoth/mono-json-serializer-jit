47fbbefa3a5dab680f0265a2ce0df1add07634a4
HookedBehemoth <luisscheurenbrand@gmail.com>
2022-09-07 23:26:13 +0200

    - unit tests - minor cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ mod mono;
 use dynasmrt::*;
 use mono::*;
 
-use std::{alloc, mem, ops::Shl, ptr};
+use std::{alloc, ops::Shl, ptr, arch::asm};
 
 macro_rules! json_dynasm {
     ($ops:ident $($t:tt)*) => {
         dynasm!($ops
             ; .arch x64
-            /* r15: object, r14: buffer, r13: offset */
+
             ; .alias object, r15
             ; .alias buffer, r14
 
@@ -19,11 +19,25 @@ macro_rules! json_dynasm {
             ; .alias temp_8, r12b
             // return code/return bytecode pc
             ; .alias retval, rax
-            ; .alias retval_32, eax
-            ; .alias retval_8, al
             $($t)*
         )
     }
+}
+
+unsafe fn emit_boolean(field_offset: i32, assembler: &mut Assembler<x64::X64Relocation>) {
+    json_dynasm!(assembler
+        ; mov temp_8, BYTE [object + field_offset]
+        ; test temp_8, temp_8
+        ; je >not
+        ; mov DWORD [buffer], 0x65757274
+        ; add buffer, 4
+        ; jmp >exit
+        ;not:
+        ; mov DWORD [buffer], 0x736C6166
+        ; mov DWORD [buffer + 1], 0x65736C61
+        ; add buffer, 5
+        ; exit:
+    );
 }
 
 unsafe fn push_utf16(s: *const MonoString, dst: *mut u8) -> usize {
@@ -63,6 +77,26 @@ macro_rules! emit_integer {
             ; pop rdi
         )
     };
+}
+
+fn emit_float(field_offset: i32, assembler: &mut Assembler<x64::X64Relocation>) {
+    json_dynasm!(assembler
+        ; movss xmm0, DWORD [object + field_offset]
+        ; mov rdi, buffer
+        ; mov rax, QWORD push_float::<f32> as _
+        ; call rax
+        ; add buffer, retval
+    );
+}
+
+fn emit_double(field_offset: i32, assembler: &mut Assembler<x64::X64Relocation>) {
+    json_dynasm!(assembler
+        ; movsd xmm0, QWORD [object + field_offset]
+        ; mov rdi, buffer
+        ; mov rax, QWORD push_float::<f64> as _
+        ; call rax
+        ; add buffer, retval
+    );
 }
 
 fn emit_null(assembler: &mut Assembler<x64::X64Relocation>) {
@@ -133,19 +167,7 @@ unsafe fn emit_serialize_value(
 ) {
     match (*typ).typ {
         MonoTypeEnum::MONO_TYPE_BOOLEAN => {
-            json_dynasm!(assembler
-                ; mov temp_8, BYTE [object + field_offset]
-                ; test temp_8, temp_8
-                ; je >not
-                ; mov DWORD [buffer], 0x65757274
-                ; add buffer, 4
-                ; jmp >exit
-                ;not:
-                ; mov DWORD [buffer], 0x736C6166
-                ; mov DWORD [buffer + 1], 0x65736C61
-                ; add buffer, 5
-                ; exit:
-            );
+            emit_boolean(field_offset, assembler);
         }
         MonoTypeEnum::MONO_TYPE_CHAR => {
             if cfg!(feature = "utf") {
@@ -198,26 +220,10 @@ unsafe fn emit_serialize_value(
             emit_integer!(u64, field_offset, rdi, QWORD, assembler);
         }
         MonoTypeEnum::MONO_TYPE_R4 => {
-            json_dynasm!(assembler
-                ; push rdi
-                ; movss xmm0, DWORD [object + field_offset]
-                ; mov rdi, buffer
-                ; mov rax, QWORD push_float::<f32> as _
-                ; call rax
-                ; add buffer, retval
-                ; pop rdi
-            );
+            emit_float(field_offset, assembler);
         }
         MonoTypeEnum::MONO_TYPE_R8 => {
-            json_dynasm!(assembler
-                ; push rdi
-                ; movsd xmm0, QWORD [object + field_offset]
-                ; mov rdi, buffer
-                ; mov rax, QWORD push_float::<f64> as _
-                ; call rax
-                ; add buffer, retval
-                ; pop rdi
-            );
+            emit_double(field_offset, assembler);
         }
         MonoTypeEnum::MONO_TYPE_STRING => {
             json_dynasm!(assembler
@@ -299,11 +305,7 @@ unsafe fn emit_serialize_class(
 
         json_dynasm!(assembler
             ; push object
-        );
-
-        emit_serialize_value(typ, (*field).offset, assembler);
-
-        json_dynasm!(assembler
+            ;;emit_serialize_value(typ, (*field).offset, assembler)
             ; pop object
         );
     }
@@ -339,91 +341,16 @@ pub unsafe extern "C" fn emit_length(obj: *const MonoObject) -> *mut ExecutableB
 pub unsafe extern "C" fn emit(obj: *const MonoObject) -> *mut ExecutableBuffer {
     let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
 
-    json_dynasm!(assembler
-        ; push rbx
-        ; push rsp
-        ; push rbp
-        ; push r12
-        ; push r13
-        ; push r14
-        ; push r15
-        ; push rsi
-        ; mov object, rdi
-        ; mov buffer, rsi
-    );
-
     let klass = mono_object_get_class(obj);
 
-    /*
-    emit_string_copy(
-        "VeryLongStringThatIJustMadeUpIdkWhyIMakeItSoLong133",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "\n",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "1",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "\n",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "12",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "\n",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "123",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "\n",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "1234",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "\n",
-        &mut assembler,
-    );
-
-    emit_string_copy(
-        "12345",
-        &mut assembler,
-    );
-    */
-
-    emit_serialize_class(klass, &mut assembler);
-
     json_dynasm!(assembler
+        /* Store buffer start */
+        ; push buffer
+        ;;emit_serialize_class(klass, &mut assembler)
+        /* Calculate buffer length */
         ; pop rsi
         ; sub buffer, rsi
         ; mov retval, buffer
-        ; pop r15
-        ; pop r14
-        ; pop r13
-        ; pop r12
-        ; pop rbp
-        ; pop rsp
-        ; pop rbx
         ; ret
     );
 
@@ -447,9 +374,18 @@ pub unsafe extern "C" fn invoke(
     obj: *const MonoObject,
     buffer: *mut u8,
 ) -> usize {
-    let serialize: extern "cdecl" fn(obj: *const MonoObject, buffer: *mut u8) -> usize =
-        mem::transmute((*code).as_ptr());
-    serialize(obj, buffer)
+    let ret: usize;
+
+    asm!(
+        "call rcx",
+
+        in("r14") buffer,
+        in("r15") obj,
+        in("rcx") (*code).as_ptr(),
+        out("rax") ret,
+    );
+
+    ret
 }
 
 #[no_mangle]
@@ -461,14 +397,301 @@ pub unsafe extern "C" fn destroy(code: *mut ExecutableBuffer) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::{slice, str};
+
+    macro_rules! newline {
+        ($assembler:ident) => {
+            emit_string_copy("\n", &mut $assembler)
+        };
+    }
+
+    macro_rules! gen_test {
+        ($value:expr, $exp:expr, $assembler:ident) => {
+            json_dynasm!($assembler
+                ; ret
+            );
+
+            let code = $assembler.finalize().unwrap();
+
+            let mut buffer = vec![0u8; 0x1000];
+            let mut offset: usize = buffer.as_mut_ptr() as usize;
+            unsafe {
+                asm!(
+                    "call rcx",
+
+                    in("rcx") code.as_ptr() as usize,
+                    inout("r14") offset,
+                    in("r15") $value
+                );
+                let length = offset - buffer.as_ptr() as usize;
+                let slice = slice::from_raw_parts(buffer.as_ptr(), length);
+                let string = str::from_utf8(slice).unwrap();
+
+                assert_eq!($exp, string);
+            }
+        };
+    }
 
     #[test]
-    fn it_works() {
-        unsafe {
-            let code = emit(ptr::null());
-            let result = invoke(code, ptr::null(), ptr::null_mut());
-            assert_eq!(result, 42);
-            destroy(code);
-        }
+    fn null() {
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        emit_null(&mut assembler);
+
+        gen_test!(0 as usize, "null", assembler);
+    }
+
+    #[test]
+    fn integers() {
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[i8] = &[i8::MIN, -1, 0, 1, i8::MAX];
+        let width = 1;
+
+        emit_integer!(i8, 0 * width, dil, BYTE, assembler);
+        newline!(assembler);
+        emit_integer!(i8, 1 * width, dil, BYTE, assembler);
+        newline!(assembler);
+        emit_integer!(i8, 2 * width, dil, BYTE, assembler);
+        newline!(assembler);
+        emit_integer!(i8, 3 * width, dil, BYTE, assembler);
+        newline!(assembler);
+        emit_integer!(i8, 4 * width, dil, BYTE, assembler);
+
+        gen_test!(value.as_ptr() as usize, "-128\n-1\n0\n1\n127", assembler);
+
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[i16] = &[i16::MIN, -1, 0, 1, i16::MAX];
+        let width = 2;
+
+        emit_integer!(i16, 0 * width, di, WORD, assembler);
+        newline!(assembler);
+        emit_integer!(i16, 1 * width, di, WORD, assembler);
+        newline!(assembler);
+        emit_integer!(i16, 2 * width, di, WORD, assembler);
+        newline!(assembler);
+        emit_integer!(i16, 3 * width, di, WORD, assembler);
+        newline!(assembler);
+        emit_integer!(i16, 4 * width, di, WORD, assembler);
+
+        gen_test!(
+            value.as_ptr() as usize,
+            "-32768\n-1\n0\n1\n32767",
+            assembler
+        );
+
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[i32] = &[i32::MIN, -1, 0, 1, i32::MAX];
+        let width = 4;
+
+        emit_integer!(i32, 0 * width, edi, DWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i32, 1 * width, edi, DWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i32, 2 * width, edi, DWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i32, 3 * width, edi, DWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i32, 4 * width, edi, DWORD, assembler);
+
+        gen_test!(
+            value.as_ptr() as usize,
+            "-2147483648\n-1\n0\n1\n2147483647",
+            assembler
+        );
+
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[i64] = &[i64::MIN, -1, 0, 1, i64::MAX];
+        let width = 8;
+
+        emit_integer!(i64, 0 * width, rdi, QWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i64, 1 * width, rdi, QWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i64, 2 * width, rdi, QWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i64, 3 * width, rdi, QWORD, assembler);
+        newline!(assembler);
+        emit_integer!(i64, 4 * width, rdi, QWORD, assembler);
+
+        gen_test!(
+            value.as_ptr() as usize,
+            "-9223372036854775808\n-1\n0\n1\n9223372036854775807",
+            assembler
+        );
+
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[u8] = &[u8::MIN, 42, u8::MAX];
+        let width = 1;
+
+        emit_integer!(u8, 0 * width, dil, BYTE, assembler);
+        newline!(assembler);
+        emit_integer!(u8, 1 * width, dil, BYTE, assembler);
+        newline!(assembler);
+        emit_integer!(u8, 2 * width, dil, BYTE, assembler);
+
+        gen_test!(value.as_ptr() as usize, "0\n42\n255", assembler);
+
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[u16] = &[u16::MIN, 1337, u16::MAX];
+        let width = 2;
+
+        emit_integer!(u16, 0 * width, di, WORD, assembler);
+        newline!(assembler);
+        emit_integer!(u16, 1 * width, di, WORD, assembler);
+        newline!(assembler);
+        emit_integer!(u16, 2 * width, di, WORD, assembler);
+
+        gen_test!(value.as_ptr() as usize, "0\n1337\n65535", assembler);
+
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[u32] = &[u32::MIN, 1337, u32::MAX];
+        let width = 4;
+
+        emit_integer!(u32, 0 * width, edi, DWORD, assembler);
+        newline!(assembler);
+        emit_integer!(u32, 1 * width, edi, DWORD, assembler);
+        newline!(assembler);
+        emit_integer!(u32, 2 * width, edi, DWORD, assembler);
+
+        gen_test!(value.as_ptr() as usize, "0\n1337\n4294967295", assembler);
+
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[u64] = &[u64::MIN, 1337, u64::MAX];
+        let width = 8;
+
+        emit_integer!(u64, 0 * width, rdi, QWORD, assembler);
+        newline!(assembler);
+        emit_integer!(u64, 1 * width, rdi, QWORD, assembler);
+        newline!(assembler);
+        emit_integer!(u64, 2 * width, rdi, QWORD, assembler);
+
+        gen_test!(
+            value.as_ptr() as usize,
+            "0\n1337\n18446744073709551615",
+            assembler
+        );
+    }
+
+    #[test]
+    fn floats() {
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[f32] = &[
+            0.0,
+            0.12345,
+            1.2345,
+            12.345,
+            123.45,
+            1234.5,
+            12345.0,
+            f32::NAN,
+            f32::INFINITY,
+            f32::NEG_INFINITY,
+        ];
+
+        emit_float(0 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(1 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(2 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(3 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(4 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(5 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(6 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(7 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(8 * 4, &mut assembler);
+        newline!(assembler);
+        emit_float(9 * 4, &mut assembler);
+
+        gen_test!(
+            value.as_ptr() as usize,
+            "0.0\n0.12345\n1.2345\n12.345\n123.45\n1234.5\n12345.0\nNaN\ninf\n-inf",
+            assembler
+        );
+    }
+
+    #[test]
+    fn doubles() {
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        let value: &[f64] = &[
+            0.0,
+            0.12345,
+            1.2345,
+            12.345,
+            123.45,
+            1234.5,
+            12345.0,
+            f64::NAN,
+            f64::INFINITY,
+            f64::NEG_INFINITY,
+        ];
+
+        emit_double(0 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(1 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(2 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(3 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(4 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(5 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(6 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(7 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(8 * 8, &mut assembler);
+        newline!(assembler);
+        emit_double(9 * 8, &mut assembler);
+
+        gen_test!(
+            value.as_ptr() as usize,
+            "0.0\n0.12345\n1.2345\n12.345\n123.45\n1234.5\n12345.0\nNaN\ninf\n-inf",
+            assembler
+        );
+    }
+
+    #[test]
+    fn strings() {
+        let mut assembler = dynasmrt::x64::Assembler::new().unwrap();
+
+        emit_string_copy("Some random longer string", &mut assembler);
+        newline!(assembler);
+        emit_string_copy("1", &mut assembler);
+        newline!(assembler);
+        emit_string_copy("12", &mut assembler);
+        newline!(assembler);
+        emit_string_copy("123", &mut assembler);
+        newline!(assembler);
+        emit_string_copy("1234", &mut assembler);
+        newline!(assembler);
+        emit_string_copy("12345", &mut assembler);
+        newline!(assembler);
+        emit_string_copy("123456", &mut assembler);
+        newline!(assembler);
+        emit_string_copy("1234567", &mut assembler);
+
+        gen_test!(
+            0 as usize,
+            "Some random longer string\n1\n12\n123\n1234\n12345\n123456\n1234567",
+            assembler
+        );
     }
 }
