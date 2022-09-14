6694c3299d7fab44eedd49686c38a36df3e80ef5
HookedBehemoth <LuisScheurenbrand@gmail.com>
2022-09-14 18:01:06 +0200

    use direct float formating

--- src/float.rs
+++ src/float.rs
@@ -1,14 +1,15 @@
 use crate::{json_dynasm, strings::emit_string_copy, Assembler};
 use dynasmrt::*;
 
-pub unsafe extern "sysv64" fn push_float<F: ryu::Float>(value: F, dst: *mut u8) -> usize {
-    let mut buffer = ryu::Buffer::new();
-    let printed = buffer.format(value);
-    std::ptr::copy_nonoverlapping(printed.as_ptr(), dst, printed.len());
-    printed.len()
+pub unsafe extern "sysv64" fn push_f32(value: f32, dst: *mut u8) -> usize {
+    ryu::raw::format32(value, dst)
 }
 
-pub unsafe extern "sysv64" fn calc_float_size<F: ryu::Float>(value: F) -> usize {
+pub unsafe extern "sysv64" fn push_f64(value: f64, dst: *mut u8) -> usize {
+    ryu::raw::format64(value, dst)
+}
+
+pub unsafe extern "sysv64" fn calc_float_size<F: ryu::Float + std::fmt::Display>(value: F) -> usize {
     let mut buffer = ryu::Buffer::new();
     let printed = buffer.format(value);
     printed.len()
@@ -70,7 +71,7 @@ pub fn emit_f32(field_offset: i32, assembler: &mut Assembler) {
 
         ;some:
         ; mov rdi, buffer
-        ; mov temp, QWORD push_float::<f32> as _
+        ; mov temp, QWORD push_f32 as _
         ; call temp
         ; add buffer, retval
 
@@ -92,7 +93,7 @@ pub fn emit_f64(field_offset: i32, assembler: &mut Assembler) {
 
         ;some:
         ; mov rdi, buffer
-        ; mov temp, QWORD push_float::<f64> as _
+        ; mov temp, QWORD push_f64 as _
         ; call temp
         ; add buffer, retval
 
